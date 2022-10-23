e7101f1876fe3fd6c95721049051210a3410da16
Mingjie Shang <mingjie.shang@sktlab.com>
2022-10-23 14:45:52 +0800

    change base map

--- js/esri-map.js
+++ js/esri-map.js
@@ -20,7 +20,7 @@ WildRydes.map = WildRydes.map || {};
     ) {
         var wrMap = WildRydes.map;
 
-        var map = new Map({ basemap: 'gray-vector' });
+        var map = new Map({ basemap: '0fH4mL2' });
 
         var view = new MapView({
             center: [-122.31, 47.60],
